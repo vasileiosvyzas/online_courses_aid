3230ebcceb75f7e49980863785d2c50da729c01e
vasileiosvyzas <vizasb@gmail.com>
2023-06-30 17:51:33 +0100

    I think I have a very rough front end now

--- streamlit/app.py
+++ streamlit/app.py
@@ -1,30 +1,110 @@
 import streamlit as st
 import sys
+import os
 from elasticsearch import Elasticsearch
+from elasticsearch_dsl import Search
+from dotenv import load_dotenv, find_dotenv
 import utils
 import templates
+import os
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+from dotenv import load_dotenv, find_dotenv
+import google_auth_oauthlib.flow
+import googleapiclient.discovery
+import googleapiclient.errors
+
+from googleapiclient.http import MediaIoBaseDownload
+
+# find .env automagically by walking up directories until it's found
+dotenv_path = find_dotenv()
+
+# load up the entries as environment variables
+load_dotenv(dotenv_path)
+
+DEVELOPER_KEY = os.environ.get("YOUTUBE_DEVELOPER_KEY")
+YOUTUBE_API_SERVICE_NAME = 'youtube'
+YOUTUBE_API_VERSION = 'v3'
+ELASTIC_PASSWORD = os.environ.get("ELASTIC_USER_PASSWORD")
 
 INDEX = 'udemy_data'
 DOMAIN = '0.0.0.0'
-es = Elasticsearch(host=DOMAIN)
-utils.check_and_create_index(es, INDEX)
+es = Elasticsearch(
+    hosts="https://localhost:9200",
+    ca_certs="../http_ca.crt",
+    http_auth=("elastic", ELASTIC_PASSWORD),
+    use_ssl=True,
+    verify_certs=False
+)
+
+def youtube_search(q, max_results):
+    youtube = build(
+        YOUTUBE_API_SERVICE_NAME,
+        YOUTUBE_API_VERSION, 
+        developerKey=DEVELOPER_KEY)
+
+    # Call the search.list method to retrieve results matching the specified query term.
+    search_response = youtube.search().list(
+        q=q,
+        part='id,snippet',
+        maxResults=max_results,
+    ).execute()
+
+    videos = []
+    video_details = []
+    channels = []
+    playlists = []
+
+    # Add each result to the appropriate list, and then display the lists of
+    # matching videos, channels, and playlists.
+    for search_result in search_response.get('items', []):
+        if search_result['id']['kind'] == 'youtube#video':
+            video_details.append(
+                {
+                    'title': search_result['snippet']['title'], 
+                    'video': search_result['id']['videoId'], 
+                    'description': search_result['snippet']['description']
+                }
+            )
+    return video_details
+
+def search_result(i: int, url: str, title: str, headline: str, **kwargs) -> str:
+    """ HTML scripts to display search results. """
+    return f"""
+        <div style="font-size:120%;">
+            {i + 1}.
+            <a href="{url}">
+                {title}
+            </a>          
+        </div>
+        <div style="font-size:95%;">
+         <div style="color:grey;font-size:95%;">
+                {url[:90] + '...' if len(url) > 100 else url}
+            </div>
+            <div style="float:left;font-style:italic;">
+                {headline} ·&nbsp;
+            </div>
+        </div>
+    """
 
 def main():
     st.title('Search a Udemy course')
     search = st.text_input('Enter search words:')
     
     if search:
-        results = utils.index_search(es, INDEX, search, None, 0, 5)
-        print(results)
-        total_hits = results['aggregations']['match_count']['value']
-        
+        s = Search(using=es, index=INDEX).query("match", title=search)
+        response = s.execute()
+
         # search results
-        # for i in range(len(results['hits']['hits'])):
-        #     result = results['hits']['hits'][i]
-        #     res = result['_source']
-        #     res['url'] = result['_id']
-        #     res['highlights'] = '...'.join(result['highlight']['content'])
-        #     st.write(templates.search_result(i, **res), unsafe_allow_html=True)
+        for index, hit in enumerate(response):
+            videos_list = youtube_search(q=hit.headline, max_results=5)
+
+            st.header('Udemy Course')
+            st.write(search_result(i=index, url=f'https://www.udemy.com{hit.url}', title=hit.title, headline=hit.headline), unsafe_allow_html=True)
+            st.subheader('Related YouTube videos')
+            for i, video in enumerate(videos_list):
+                st.write(search_result(i=i, url=f"https://www.youtube.com/{video['video']}", title=video['title'], headline=video['description']), unsafe_allow_html=True)
+
 
 if __name__ == '__main__':
     main()
