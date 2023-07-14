4ab090f32a2deef8d0818f1d8cf3f0aab19606dc
vasileiosvyzas <vizasb@gmail.com>
2023-07-14 16:35:47 +0100

    committing again failed hooks

--- setup.py
+++ setup.py
@@ -6,12 +6,8 @@ from setuptools import find_namespace_packages, setup
 BASE_DIR = Path(__file__).parent
 with open(Path(BASE_DIR, "requirements.txt")) as file:
     required_packages = [ln.strip() for ln in file.readlines()]
-    
-style_packages = [
-    "black==23.7.0",
-    "flake8==6.0.0",
-    "isort==5.12.0"
-]
+
+style_packages = ["black==23.7.0", "flake8==6.0.0", "isort==5.12.0"]
 
 # Define our package
 setup(
@@ -21,5 +17,5 @@ setup(
     python_requires=">=3.7",
     packages=find_namespace_packages(),
     install_requires=[required_packages],
-    extras_require={"dev": style_packages + ["pre-commit==3.3.2"]},
+    extras_require={"dev": style_packages + ["pre-commit==3.3.3"]},
 )
--- src/udemy_client.py
+++ src/udemy_client.py
@@ -1,21 +1,19 @@
-import requests
 import os
 import time
-import pandas as pd
-
 from typing import Dict, List
-from requests.sessions import Session
+
+import requests
+from dotenv import find_dotenv, load_dotenv
 from requests.auth import HTTPBasicAuth
-from dotenv import load_dotenv, find_dotenv
-from tenacity import retry, wait_exponential
+from requests.sessions import Session
 
 
 class UdemyClient:
     """
-        Class that contains functions for requesting and accessing the data from the Udemy API
-        https://www.udemy.com/developers/affiliate/
+    Class that contains functions for requesting and accessing the data from the Udemy API
+    https://www.udemy.com/developers/affiliate/
     """
-    
+
     def __init__(self) -> None:
         # find .env automagically by walking up directories until it's found
         dotenv_path = find_dotenv()
@@ -25,7 +23,7 @@ class UdemyClient:
 
         self.CLIENT_ID = os.environ.get("UDEMY_CLIENT_ID")
         self.CLIENT_SECRET = os.environ.get("UDEMY_CLIENT_SECRET")
-    
+
     def request_data(self, url: str, current_session: Session) -> Dict:
         """Simple function that makes a get request from a url provided and returns the response
 
@@ -34,15 +32,17 @@ class UdemyClient:
             current_session (Session): establishing a session so that to use the same TCP connection
 
         Returns:
-            response: the response payload for the request 
+            response: the response payload for the request
         """
-        
+
         try:
-            response = current_session.get(url=url, auth=HTTPBasicAuth(self.CLIENT_ID, self.CLIENT_SECRET), timeout=10)
+            response = current_session.get(
+                url=url, auth=HTTPBasicAuth(self.CLIENT_ID, self.CLIENT_SECRET), timeout=10
+            )
             response.raise_for_status()
-            
+
             return response.json()
-        
+
         except requests.exceptions.HTTPError as errh:
             print(errh)
             raise
@@ -55,7 +55,7 @@ class UdemyClient:
         except requests.exceptions.RequestException as err:
             print(err)
             raise
-        
+
     def has_next_page(self, response: Dict) -> bool:
         """Simple utility function to check the response for a next page url
 
@@ -65,59 +65,61 @@ class UdemyClient:
         Returns:
             bool: True if there is a next page and False if there is not
         """
-        
-        if response['next'] is not None:
+
+        if response["next"] is not None:
             return True
         else:
             return False
-    
+
     def get_courses_list(self) -> List:
         """
         This function requests data from the course-list endpoint and gets all the results for each particular page
         then it requests the data for the next page and saves all the results in a list
-        
+
         Returns:
             List: Big list of results with all the courses
-            
+
         TODO: implement rate limitting and retry in case of failures
         TODO: decide on an appropriate page size
         """
         session = requests.Session()
 
-        courses_list_endpoint = 'https://www.udemy.com/api-2.0/courses/?page_size=6'
+        courses_list_endpoint = "https://www.udemy.com/api-2.0/courses/?page_size=6"
 
         # get the list of courses available on Udemy
         response = self.request_data(url=courses_list_endpoint, current_session=session)
-        courses_list = response['results']
+        courses_list = response["results"]
         print(response)
-        
+
         while self.has_next_page(response=response):
-            print(response['next'])
+            print(response["next"])
             time.sleep(10)
-            response = self.request_data(url=response['next'], current_session=session)
-            courses_list.extend(response['results'])
-            
+            response = self.request_data(url=response["next"], current_session=session)
+            courses_list.extend(response["results"])
+
         return courses_list
-    
+
     def get_course_details(self, course_id: str) -> Dict:
         """
         This function is responsible for requesting the details for a specific course from the API
-        
+
         Args:
             course_id (str): the course we want the course details for
-        
+
         Returns:
             Dict: A dictionary with the course details
         """
-        
+
         session = requests.Session()
         course_details_endpoint = f"https://www.udemy.com/api-2.0/courses/{course_id}/?fields[course]=title,headline,description,url,visible_instructors,primary_category,primary_subcategory,status_label"
-        
-        single_courses_details = self.request_data(url=course_details_endpoint, current_session=session)
+
+        single_courses_details = self.request_data(
+            url=course_details_endpoint, current_session=session
+        )
         return single_courses_details
-    
+
     def get_course_curriculum(self, course_id: str) -> List:
-        """Gets a course id and returns the curriculum for this course by going through 
+        """Gets a course id and returns the curriculum for this course by going through
         every page and storing the results in a list
 
         Args:
@@ -126,18 +128,17 @@ class UdemyClient:
         Returns:
             List: a list with all the results from all the pages for this specific course
         """
-        
+
         session = requests.Session()
-        course_curriculum_endpoint = f'https://www.udemy.com/api-2.0/courses/{course_id}/public-curriculum-items/?page_size=6'
-        
-        response = self.request_data(url=course_curriculum_endpoint, current_session=session)        
-        courses_curriculum = response['results']
-        
+        course_curriculum_endpoint = f"https://www.udemy.com/api-2.0/courses/{course_id}/public-curriculum-items/?page_size=6"
+
+        response = self.request_data(url=course_curriculum_endpoint, current_session=session)
+        courses_curriculum = response["results"]
+
         while self.has_next_page(response=response):
-            next_page = response['next']
+            next_page = response["next"]
             time.sleep(2)
             response = self.request_data(url=next_page, current_session=session)
-            courses_curriculum.extend(response['results'])
-        
+            courses_curriculum.extend(response["results"])
+
         return courses_curriculum
-    
